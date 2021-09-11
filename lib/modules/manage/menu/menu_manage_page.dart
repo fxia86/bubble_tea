9b70b971608ef63b9f31c9cd5978400f1949c78b
xiafeng <81973175@qq.com>
2021-09-11 16:47:18 +0800

    reorderable gridview

--- lib/modules/manage/menu/menu_manage_page.dart
+++ lib/modules/manage/menu/menu_manage_page.dart
@@ -1,10 +1,11 @@
-import 'package:bubble_tea/data/local/local_storage.dart';
+// import 'package:bubble_tea/data/local/local_storage.dart';
 import 'package:bubble_tea/routes/pages.dart';
 import 'package:bubble_tea/widgets/body_layout.dart';
 import 'package:bubble_tea/widgets/my_icon_button.dart';
+import 'package:drag_and_drop_gridview/devdrag.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:reorderables/reorderables.dart';
+// import 'package:reorderables/reorderables.dart';
 
 import 'menu_manage_controller.dart';
 
@@ -74,32 +75,46 @@ class ReorderMenu extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    bool collapsed = LocalStorage.getNaviBarcollapsed();
-    return SingleChildScrollView(
-      child: ReorderableWrap(
-        spacing: 12,
-        runSpacing: 12,
-        padding: const EdgeInsets.symmetric(horizontal: 5, vertical: 20),
-        children: [
-          for (var item in list)
-            Container(
-                width: Get.width * (collapsed ? 0.295 : 0.228),
-                height: Get.width * (collapsed ? 0.295 : 0.245),
-                child: MenuItem(item: item, deletable: true))
-        ],
-        onReorder: (int oldIndex, int newIndex) =>
-            controller.reorder(oldIndex, newIndex, list.toList()[0].catalogId),
-        // onNoReorder: (int index) {
-        //   //this callback is optional
-        //   debugPrint(
-        //       '${DateTime.now().toString().substring(5, 22)} reorder cancelled. index:$index');
-        // },
-        // onReorderStarted: (int index) {
-        //   //this callback is optional
-        //   debugPrint(
-        //       '${DateTime.now().toString().substring(5, 22)} reorder started: index:$index');
-        // },
+    // bool collapsed = LocalStorage.getNaviBarcollapsed();
+    // return SingleChildScrollView(
+    //   child: ReorderableWrap(
+    //     spacing: 12,
+    //     runSpacing: 12,
+    //     padding: const EdgeInsets.symmetric(horizontal: 5, vertical: 20),
+    //     children: [
+    //       for (var item in list)
+    //         Container(
+    //             width: Get.width * (collapsed ? 0.295 : 0.228),
+    //             height: Get.width * (collapsed ? 0.295 : 0.245),
+    //             child: MenuItem(item: item, deletable: true))
+    //     ],
+    //     onReorder: (int oldIndex, int newIndex) =>
+    //         controller.reorder(oldIndex, newIndex, list.toList()[0].catalogId),
+    //     // onNoReorder: (int index) {
+    //     //   //this callback is optional
+    //     //   debugPrint(
+    //     //       '${DateTime.now().toString().substring(5, 22)} reorder cancelled. index:$index');
+    //     // },
+    //     // onReorderStarted: (int index) {
+    //     //   //this callback is optional
+    //     //   debugPrint(
+    //     //       '${DateTime.now().toString().substring(5, 22)} reorder started: index:$index');
+    //     // },
+    //   ),
+    // );
+    return DragAndDropGridView(
+      padding: EdgeInsets.symmetric(horizontal: 5, vertical: 20),
+      onWillAccept: (int oldIndex, int newIndex) => oldIndex != newIndex,
+      onReorder: (int oldIndex, int newIndex) =>
+          controller.reorder(oldIndex, newIndex, list.toList()[0].catalogId),
+      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 3,
+        mainAxisSpacing: 12,
+        crossAxisSpacing: 12,
+        // childAspectRatio: 0.9,
       ),
+      itemCount: list.length,
+      itemBuilder: (c, i) => MenuItem(item: list.elementAt(i)),
     );
   }
 }
@@ -124,23 +139,10 @@ class MenuGrid extends StatelessWidget {
       itemCount: list.length,
       itemBuilder: (c, i) => MenuItem(item: list.elementAt(i)),
     );
-    // DragAndDropGridView(
-    //         padding: EdgeInsets.symmetric(horizontal: 5, vertical: 20),
-    //         onWillAccept: (int oldIndex, int newIndex) => oldIndex != newIndex,
-    //         onReorder: (int oldIndex, int newIndex) => controller.reorder(
-    //             oldIndex, newIndex, list.toList()[0].catalogId),
-    //         gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-    //           crossAxisCount: crossAxisCount,
-    //           mainAxisSpacing: 12,
-    //           crossAxisSpacing: 12,
-    //           // childAspectRatio: 0.9,
-    //         ),
-    //         itemCount: list.length,
-    //         itemBuilder: (c, i) => MenuItem(item: list.elementAt(i)),
-    //       );
   }
 }
 
+// ignore: must_be_immutable
 class MenuItem extends StatelessWidget {
   MenuItem({Key? key, required this.item, this.deletable = false})
       : super(key: key);
@@ -148,6 +150,10 @@ class MenuItem extends StatelessWidget {
 
   final item;
   final bool deletable;
+
+  int variableSet = 0;
+  double height = 0;
+  double width = 0;
 
   @override
   Widget build(BuildContext context) {
@@ -158,67 +164,77 @@ class MenuItem extends StatelessWidget {
       clipBehavior: Clip.antiAlias,
       // shape: ShapeBorder(),
       child: InkWell(
-        onTap: () => Get.toNamed(Routes.MANAGE_MENU_DISH, arguments: item.id),
-        // onLongPress: () => controller.deleteConfirm(item.id),
-        // Generally, material cards use onSurface with 12% opacity for the pressed state.
-        splashColor: Get.theme.colorScheme.onSurface.withOpacity(0.12),
-        // Generally, material cards do not have a highlight overlay.
-        highlightColor: Colors.transparent,
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            SizedBox(
-              height: Get.height * 0.25,
-              child: Ink.image(
-                image: NetworkImage(item.img),
-                fit: BoxFit.cover,
-                child: Container(),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.fromLTRB(10, 10, 10, 0),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Expanded(
-                      child: Padding(
-                    padding: const EdgeInsets.only(right: 10),
-                    child: Text(
-                      item.name,
-                      style: Get.textTheme.headline5
-                          ?.copyWith(fontWeight: FontWeight.w500),
-                    ),
-                  )),
-                  Text("€ ${(item.price / 100).toStringAsFixed(2)}",
-                      style: Get.textTheme.headline5),
-                ],
-              ),
-            ),
-            Expanded(
-              child: Padding(
-                padding: const EdgeInsets.fromLTRB(10, 5, 0, 5),
-                child: Row(
+          onTap: () => Get.toNamed(Routes.MANAGE_MENU_DISH, arguments: item.id),
+          // onLongPress: () => controller.deleteConfirm(item.id),
+          // Generally, material cards use onSurface with 12% opacity for the pressed state.
+          splashColor: Get.theme.colorScheme.onSurface.withOpacity(0.12),
+          // Generally, material cards do not have a highlight overlay.
+          highlightColor: Colors.transparent,
+          child: LayoutBuilder(builder: (context, costrains) {
+            if (variableSet == 0) {
+              width = costrains.maxWidth;
+              height = costrains.maxHeight;
+              variableSet++;
+            }
+            return Container(
+                width: width,
+                height: height,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    Expanded(
-                      child: Text(
-                        item.desc ?? "",
-                        style: Get.textTheme.subtitle1,
-                        overflow: TextOverflow.fade,
+                    SizedBox(
+                      height: Get.height * 0.25,
+                      child: Ink.image(
+                        image: NetworkImage(item.img),
+                        fit: BoxFit.cover,
+                        child: Container(),
                       ),
                     ),
-                    if (deletable)
-                      ScaleIconButton(
-                        onPressed: () => controller.deleteConfirm(item.id),
-                        icon: Icon(Icons.delete),
-                        color: Colors.red,
+                    Padding(
+                      padding: const EdgeInsets.fromLTRB(10, 10, 10, 0),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Expanded(
+                              child: Padding(
+                            padding: const EdgeInsets.only(right: 10),
+                            child: Text(
+                              item.name,
+                              style: Get.textTheme.headline5
+                                  ?.copyWith(fontWeight: FontWeight.w500),
+                            ),
+                          )),
+                          Text("€ ${(item.price / 100).toStringAsFixed(2)}",
+                              style: Get.textTheme.headline5),
+                        ],
                       ),
+                    ),
+                    Expanded(
+                      child: Padding(
+                        padding: const EdgeInsets.fromLTRB(10, 5, 0, 5),
+                        child: Row(
+                          children: [
+                            Expanded(
+                              child: Text(
+                                item.desc ?? "",
+                                style: Get.textTheme.subtitle1,
+                                overflow: TextOverflow.fade,
+                              ),
+                            ),
+                            if (deletable)
+                              ScaleIconButton(
+                                onPressed: () =>
+                                    controller.deleteConfirm(item.id),
+                                icon: Icon(Icons.delete),
+                                color: Colors.red,
+                              ),
+                          ],
+                        ),
+                      ),
+                    ),
                   ],
-                ),
-              ),
-            ),
-          ],
-        ),
-      ),
+                ));
+          })),
     );
   }
 }
